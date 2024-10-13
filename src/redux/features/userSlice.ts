a1cd21d030338dfb469b567cd119373f9ed46693
CuongBap <89674054+CuongBap@users.noreply.github.com>
2024-10-13 22:20:05 +0700

    Set up Redux, match API Login

---
+++ src/redux/features/userSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { User } from "../../models/user";
+
+
+// default value
+const initialState : null | User = null
+
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        login: (state, action) => action.payload,
+        logout: () => initialState, // null
+    },
+})
+
+export const {login, logout} = userSlice.actions;
+export default userSlice.reducer;
