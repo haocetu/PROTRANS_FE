dad458152c5e5ed2facb665c0bb6b27e76fd6e4b
haocetu <nhathao230502@gmail.com>
2024-11-08 00:14:07 +0700

    Truyền vào 2 languageId để tìm translators

--- src/pages/documentDetails/index.tsx
+++ src/pages/documentDetails/index.tsx
@@ -17,6 +17,7 @@ function DocumentDetails() {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedDocumentId, setSelectedDocumentId] = useState(null);
   const [selectsecondlanguageId, setSelectsecondlanguageId] = useState(null);
+  const [selectfirstlanguageId, setSelectfirstlanguageId] = useState(null);
   const [dataAssignTrans, setDataAssignTrans] = useState([]);
   //---------------------------------------------------------
   const [loading, setLoading] = useState(false);
@@ -194,7 +195,9 @@ function DocumentDetails() {
       render: (id, data) => (
         <ContainerOutlined
           onClick={() => {
+            console.log(data.firstLanguageId);
             console.log(data.secondLanguageId);
+            setSelectfirstlanguageId(data.firstLanguageId);
             setSelectsecondlanguageId(data.secondLanguageId);
             setSelectedDocumentId(id);
             setIsOpen(true);
@@ -215,10 +218,10 @@ function DocumentDetails() {
   // }, []);
 
   //----------------------Điều Phối-----------------------------------//
-  const fetchTranslator = async (selectsecondlanguageId) => {
+  const fetchTranslator = async (selectfirstlanguageId, selectsecondlanguageId) => {
     try {
       const response = await api.get(
-        `Account/GetByLanguageId?id=${selectsecondlanguageId}`
+        `Account/GetBy2LanguageId?firstLanguageId=${selectfirstlanguageId}&secondLanguageId=${selectsecondlanguageId}`
       );
       const data = Array.isArray(response.data.data)
         ? response.data.data
@@ -237,8 +240,8 @@ function DocumentDetails() {
   };
 
   useEffect(() => {
-    fetchTranslator(selectsecondlanguageId);
-  }, [selectsecondlanguageId]);
+    fetchTranslator(selectfirstlanguageId, selectsecondlanguageId);
+  }, [selectfirstlanguageId, selectsecondlanguageId]);
   //---------------------AssignmentTranslation--------------------------------//
 
   async function fetchAssignmentTranslation() {
